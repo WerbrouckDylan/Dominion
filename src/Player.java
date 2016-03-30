c20eacd9895ea215d462ebed648faed95c0bb1e1
WerbrouckDylan <dylan.werbrouck@hotmail.com>
2016-03-30 20:50:01 +0200

    second

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,44 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Player {
     private int actions = 0;
     private int coins = 0;
     private int buys = 0;
+    private String naam;
+    private ArrayList<Card> spelerHand;
+    private ArrayList<Card> PlayArea;
+    private Stack<Card> DiscardPile;
+    private Stack<Card> Deck;
+
+    public Player(ArrayList<Card> spelerHand, String naam) {
+        this.spelerHand = spelerHand;
+        this.naam = naam;
+    }
+
+    public void takeCardsFromDeck(){
+        spelerHand.add(Deck.pop());
+    }
+
+    public void playAreaToDiscardpile(){
+        PlayArea.addAll(DiscardPile);
+    }
+
+    public Stack<Card> getDeck() {
+        return Deck;
+    }
+
+    public Stack<Card> getDiscardPile() {
+        return DiscardPile;
+    }
+
+    public ArrayList<Card> getPlayArea() {
+        return PlayArea;
+    }
+
+    public void setPlayArea(ArrayList<Card> playArea) {
+        PlayArea = playArea;
+    }
 
     public void setActions(int actions) {
         this.actions = actions;
@@ -27,14 +62,6 @@ public class Player {
 
     public void setBuys(int buys) {
         this.buys = buys;
-    }
-
-    private String naam;
-    private ArrayList<Card> spelerHand;
-
-    public Player(ArrayList<Card> spelerHand, String naam) {
-        this.spelerHand = spelerHand;
-        this.naam = naam;
     }
 
     public String getNaam() {
