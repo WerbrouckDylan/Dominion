78dbfc1a7941aa8d392da504cb5a081e717bead7
WerbrouckDylan <dylan.werbrouck@hotmail.com>
2016-04-28 16:04:54 +0200

    alle kaarten + deels abilities

---
+++ src/engine/Handles.java
@@ -0,0 +1,9 @@
+package engine;
+
+/**
+ * Created by Dolan on 21-4-2016.
+ */
+public class Handles {
+//// TODO: 21-4-2016  
+
+}
--- src/ui/ui.java
+++ src/ui/ui.java
@@ -1,11 +1,10 @@
 package ui;
 
-/**
- * Created by Dolan on 21-4-2016.
- */
 public class ui {
 
     public static void main(String[] args) {
+
+
 
     }
 }
